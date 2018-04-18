02ae4ac5f1c92d763cf0c2fcb5dd145c529a58c9
Angry Fennec <angryfennecus@gmail.com>
2018-04-18 08:05:53 +0300

    Настроена сборка, сгенерированы less-файлы

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,19 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var gulpBemCss = require('gulp-bem-css');
+
+gulp.task("html", () => {
+  return gulp.src('source/*.html')
+    .pipe(gulpBemCss({
+      folder: 'source/less', // Path for creating directories and stylesheet files.
+      extension: 'less', // Extension of stylesheet files
+      elementSeparator: '__', // Element separator in class names
+      modifierSeparator: '--' // Modifier separator in class names
+    }))
+
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
