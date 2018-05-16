fa1a5534f5bdbcefb78cd9753b3330bf96f6fce3
AngryFennec <shellfix@gmail.com>
2018-05-16 17:06:26 +0300

    JS

---
+++ source/js/script.js
@@ -0,0 +1,25 @@
+var burger = document.querySelector(".menu-button");
+var menu = document.querySelector(".page-navigation");
+var modal = document.querySelector(".modal-add");
+var modalBtn = document.querySelector(".modal-show");
+
+menu.classList.add("page-navigation--close");
+burger.classList.add("menu-button--close");
+
+burger.addEventListener ("click", function (evt) {
+  evt.preventDefault();
+  menu.classList.toggle("page-navigation--close");
+  burger.classList.toggle("menu-button--close");
+});
+
+modalBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal-add--close");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode == 27) {
+    evt.preventDefault();
+    modal.classList.add("modal-add--close");
+  }
+});
